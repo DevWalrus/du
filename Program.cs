cff90e1cd454407728726c3cba1ff624eb1d52d0
DevWalrus <70732411+DevWalrus@users.noreply.github.com>
2022-09-12 10:24:44 -0400

    Setup Interlocked method of locking the counters

--- Program.cs
+++ Program.cs
@@ -7,81 +7,121 @@ namespace du
 {
     public class DiskReader
     {
-        
-        public int FolderCount { get; private set; }
-        public int FileCount { get; private set; }
-        public long ByteCount { get; private set; }
-        
-        private readonly object folderLock = new object();
-        private readonly object fileLock = new object();
-        private readonly object byteLock = new object();
+        static int folderCount;
+        static int fileCount;
+        static long byteCount;
 
-        private void ResetCounters()
+        // private readonly object folderLock = new object();
+        // private readonly object fileLock = new object();
+        // private readonly object byteLock = new object();
+
+        /// <summary>
+        /// Initializes a new DiskReader object and sets every count to 0.
+        /// </summary>
+        public DiskReader()
         {
-            lock (folderLock)
-            {
-                FolderCount = 0;
-            }
-            
-            lock (fileLock)
-            {
-                FileCount = 0;
-            }
-            
-            lock (byteLock)
-            {
-                ByteCount = 0;
-            }
-        }
-        
-        private void IncFolder()
-        {
-            lock (folderLock)
-            {
-                FolderCount++;
-            }
-        }
-        
-        private void IncFile()
-        {
-            lock (fileLock)
-            {
-                FileCount++;
-            }
-        }
-        
-        private void AddByte(long size)
-        {
-            lock (byteLock)
-            {
-                ByteCount += size;
-            }
+            folderCount = 0;
+            fileCount = 0;
+            byteCount = 0;
         }
 
+        public void ResetCounters()
+        {
+            // lock (folderLock)
+            // {
+            //     folderCount = 0;
+            // }
+            //
+            // lock (fileLock)
+            // {
+            //     fileCount = 0;
+            // }
+            //
+            // lock (byteLock)
+            // {
+            //     byteCount = 0;
+            // }
+            Interlocked.Exchange(ref byteCount, 0);
+            Interlocked.Exchange(ref fileCount, 0);
+            Interlocked.Exchange(ref folderCount, 0);
+        }
+
+        // private void IncFolder()
+        // {
+        //     lock (folderLock)
+        //     {
+        //         folderCount++;
+        //     }
+        // }
+        //
+        // private void IncFile()
+        // {
+        //     lock (fileLock)
+        //     {
+        //         fileCount++;
+        //     }
+        // }
+        //
+        // private void AddByte(long size)
+        // {
+        //     lock (byteLock)
+        //     {
+        //         byteCount += size;
+        //     }
+        // }
+
+        /// <summary>
+        /// Look through a provided directory, <paramref name="dir"/>, and all children, to find the size of every file in the tree. The file count and
+        /// sizes are then summed and stored in the <c>fileCount</c> and <c>byteCount</c> variable, respectively. The
+        /// total folders traversed are also summed and stored in the <c>folderCount</c> variable.
+        ///
+        /// This process is done in happens in a parallel fashion, where every folder is given a new thread. This means any given thread is
+        /// going to be counting the sizes of the files in its folder and creating new threads to handle the children
+        /// files. The counterpart to this method is the <see cref="SeqDU"/> method which handles all the folders on the same thread.
+        /// </summary>
+        /// <param name="dir">The parent directory to begin the tree search through</param>
         public void ParDU(string dir)
         {
-            IncFolder();
+            //IncFolder();
+            Interlocked.Increment(ref folderCount);
             List<string> files = new List<string>();
             string[] dirs = Array.Empty<string>();
             try
             {
                 files = new List<string>(Directory.GetFiles(dir));
                 dirs = Directory.GetDirectories(dir);
-            }catch{}
+            }
+            catch (UnauthorizedAccessException ignored)
+            {
+                // Ignore these exceptions, throw all others
+            }
 
             foreach (var fInfo in files.Select(f => new FileInfo(f)))
             {
-                IncFile();
-                AddByte(fInfo.Length);
+                //IncFile();
+                Interlocked.Increment(ref fileCount);
+                //AddByte(fInfo.Length);
+                Interlocked.Add(ref byteCount, fInfo.Length);
             }
-            
+
             Parallel.ForEach(dirs, ParDU);
 
         }
-        
+
+        /// <summary>
+        /// Look through a provided directory, <paramref name="dir"/>, and all children, to find the size of every file in the tree. The file count and
+        /// sizes are then summed and stored in the <c>fileCount</c> and <c>byteCount</c> variable, respectively. The
+        /// total folders traversed are also summed and stored in the <c>folderCount</c> variable.
+        ///
+        /// This process is done in happens in a sequential fashion, where every folder parsed on the same thread.
+        /// The counterpart to this method is the <see cref="ParDU"/> method which handles each encountered folder on a
+        /// new thread.
+        /// </summary>
+        /// <param name="dir">The parent directory to begin the tree search through</param>
         public void SeqDU(string dir)
         {
-            IncFolder();
+            //IncFolder();
+            Interlocked.Increment(ref folderCount);
             List<string> files = new List<string>();
 
             string[] dirs = Array.Empty<string>();
@@ -89,37 +129,51 @@ namespace du
             {
                 files = new List<string>(Directory.GetFiles(dir));
                 dirs = Directory.GetDirectories(dir);
-            }catch{}
+            }
+            catch (UnauthorizedAccessException ignored)
+            {
+                // Ignore these exceptions, throw all others
+            }
+
             foreach (var fInfo in files.Select(f => new FileInfo(f)))
             {
-                IncFile();
-                AddByte(fInfo.Length);
+                //IncFile();
+                Interlocked.Increment(ref fileCount);
+                //AddByte(fInfo.Length);
+                Interlocked.Add(ref byteCount, fInfo.Length);
             }
-            
+
             foreach (var d in dirs)
             {
                 SeqDU(d);
             }
         }
 
-        private void Output(Stopwatch stopwatch, bool parallel)
+        public void Output(Stopwatch stopwatch, bool parallel)
         {
-            Console.WriteLine("{0} Calculated in: {1:N7}s", parallel ? "Parallel" : "Sequential", stopwatch.Elapsed.TotalMilliseconds/1000);
-            Console.WriteLine("{0:N0} folders, {1:N0} files, {2:N0} bytes", FolderCount, FileCount, ByteCount);
+            Console.WriteLine("{0} Calculated in: {1:N7}s", parallel ? "Parallel" : "Sequential",
+                stopwatch.Elapsed.TotalMilliseconds / 1000);
+            Console.WriteLine("{0:N0} folders, {1:N0} files, {2:N0} bytes", folderCount, fileCount, byteCount);
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
         public void Error()
         {
             Console.WriteLine("Usage: du [-s] [-p] [-b] <path>\n" +
-                              "Summarize disk usage of the set of FILES, recursively for directories.\n"+
-                              "You MUST specify one of the parameters, -s, -p, or -b\n"+
-                              "-s\tRun in single threaded mode\n"+
-                              "-p\tRun in parallel mode (uses all available processors)\n"+
-                              "-b\tRun in both parallel and single threaded mode.\n"+
+                              "Summarize disk usage of the set of FILES, recursively for directories.\n" +
+                              "You MUST specify one of the parameters, -s, -p, or -b\n" +
+                              "-s\tRun in single threaded mode\n" +
+                              "-p\tRun in parallel mode (uses all available processors)\n" +
+                              "-b\tRun in both parallel and single threaded mode.\n" +
                               "\tRuns parallel followed by sequential mode");
             Environment.Exit(1);
         }
-        
+    }
+
+    public class du
+    {
         /// <summary>
         /// 
         /// </summary>
@@ -155,10 +209,10 @@ namespace du
                     diskReader.ParDU(args[1]);
                     stopwatch.Stop();
                     diskReader.Output(stopwatch, true);
-                    
+
                     stopwatch.Reset();
                     diskReader.ResetCounters();
-                    
+
                     stopwatch.Start();
                     diskReader.SeqDU(args[1]);
                     stopwatch.Stop();
